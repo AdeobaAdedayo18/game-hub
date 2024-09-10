8939e305126bebe35761091c0798d2e9b4cdbbae
Adeoba Adedayo <adeobaadedayo18@gmail.com>
2024-09-10 15:16:16 +0100

    Building game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { px } from "framer-motion";
 
-const GameCard = () => {
-  interface Props {
-    game: Game;
-  }
-
-  return <div>GameCard</div>;
+interface Props {
+  game: Game;
+}
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card borderRadius={"10px"} overflow={"hidden"}>
+      <Image src={game.background_image}></Image>
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default GameCard;
