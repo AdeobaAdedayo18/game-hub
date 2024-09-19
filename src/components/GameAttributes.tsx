8d55830fd87f00af96fb5c6eeffb3c8cf450023e
Adeoba Adedayo <adeobaadedayo18@gmail.com>
2024-09-20 00:02:06 +0100

    Build game Attributes

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,36 @@
+import { SimpleGrid } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import { Game } from "../entities/Game";
+import CriticScore from "./CriticScore";
+import DefintionItems from "./DefintionItems";
+
+interface Props {
+  game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+  return (
+    <SimpleGrid columns={2} as="dl">
+      <DefintionItems term="Platforms">
+        {game.parent_platforms?.map(({ platform }) => (
+          <Text key={platform.id}>{platform.name}</Text>
+        ))}
+      </DefintionItems>
+      <DefintionItems term="Metascore">
+        <CriticScore score={game.metacritic}></CriticScore>
+      </DefintionItems>
+      <DefintionItems term="Genres">
+        {game.genres.map((genre) => (
+          <Text key={genre.id}>{genre.name}</Text>
+        ))}
+      </DefintionItems>
+      <DefintionItems term="Publishers">
+        {game.publishers?.map((publisher) => (
+          <Text key={publisher.id}>{publisher.name}</Text>
+        ))}
+      </DefintionItems>
+    </SimpleGrid>
+  );
+};
+
+export default GameAttributes;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,8 +1,11 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import ExpandableText from "../components/ExpandableText";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
+import DefintionItems from "../components/DefintionItems";
+import CriticScore from "../components/CriticScore";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -16,6 +19,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game}></GameAttributes>
     </>
   );
 };
